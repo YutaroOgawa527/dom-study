0d7089f1596e182e66d8d96bbe2b835365474066
Yutaro Ogawa <yutaro.ogawa.527@gmail.com>
2020-05-25 23:07:20 +0900

    コメント追加

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
+//dom
 const button = document.getElementById('addBtn');
 const lists = document.getElementById('lists');
 
-//Users一覧の取得
+//method
 async function getUsers() {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const users = await res.json();
   return users;
 }
 
-//Listの表示
 async function listUsers() {
   const users = await getUsers();
-  //DOM操作
-  users.forEach((user) => {
-    const list = document.createElement('li');
-    list.innerText = user.name;
-    lists.appendChild(list);
-  });
+  users.forEach(addList);
 }
 
-window.addEventListener('load', listUsers);
+function addList(user) {
+  const list = document.createElement('li');
+  list.innerText = user.name;
+  lists.appendChild(list);
+}
 
+//event
+window.addEventListener('load', listUsers);
 button.addEventListener('click', listUsers);
